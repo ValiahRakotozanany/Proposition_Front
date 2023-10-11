2738c97dae6385eb988594d531a3ab53f6c21e50
Valiah <rakotozananyvaliah8@gmail.com>
2023-10-11 17:29:37 +0300

    [Dev fron]Integration login wb

--- pages/home/Login.js
+++ pages/home/Login.js
@@ -9,13 +9,61 @@ import { TouchableOpacity } from 'react-native-gesture-handler';
 
 
 const Home = ({navigation,props}) => {
-  
+
   const data = {
     param1: 'Poulet',
     param2: 'Frites',
   };
 
-  
+  const [email, setEmail] = React.useState('');
+const [password, setPassword] = React.useState('');
+
+
+const appelWebService = async () => {
+  try {
+    console.log(email);
+    const response = await fetch(`http://192.168.88.182:8087/tiatanindrazana/Login_Famille?email=${email}&motdepasse=${password}`);
+    const data = await response.json();
+
+    console.log(data);console.log("coucou"); // Affiche les données renvoyées par le service Web dans la console
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const submit = () =>{
+  //   console.log(thijgds.state.email+" "+this.state.password);        
+   //  console.log(this.state.email);
+  /*   log.append("email",this.state.email);
+     log.append("motdepasse",this.state.password);
+     console.log(log.get("email"));*/
+  console.log(email)
+     fetch("http://26.22.221.140:8087/tiatanindrazana/Login_Famille?email="+email+"&motdepasse="+password,//"http://localhost:8087/tiatanindrazana/Login_Famille?email="+email+"&motdepasse="+password,
+     {
+         method:"POST",
+         headers : {"Content-Type":"application/json"},
+       //  body: JSON.stringify({"email":email,"motdepasse":password})
+       }) 
+       .then((res)=>{if (!res.ok) {
+       console.log('huhuu');
+        throw new Error("Network response was not ok");
+      }
+      return res.json()})
+       .then((resultat)=>{
+       if(resultat['data']!=null){
+         console.log(resultat['data'][0].token+" mandeha");            
+         navigation.navigate ('Feed');  
+        }
+        // setEnchere(resultat.data);
+     //   sessionStorage.setItem('iduser',JSON.stringify(resultat.utilisateurid))
+       // sessionStorage.setItem('token',resultat.token);  
+        
+        
+       }).catch((error) => {
+        console.error(error);
+      });
+       }
+ 
   return (
     <View>
     <ImageBackground source={require("../../assets/back1.jpg")}
@@ -33,13 +81,16 @@ const Home = ({navigation,props}) => {
        <Card.Content>   
        <Text style={{ color: 'grey', fontSize: 20, marginBottom: 10,fontWeight:'bold', letterSpacing: 3,justifyContent:'center' ,textAlign:'center' }}>Login</Text>
        <TextInput style={CardStyle.input} 
-      placeholder="Email"
+      placeholder="Email" onChangeText={text => setEmail(text)}
+      value={email}
     />             
        <TextInput style={CardStyle.input}
-         secureTextEntry={true}
+         secureTextEntry={true} 
+         onChangeText={text => setPassword(text)}
+         value={password}
        placeholder={"Mot de passe"}
     /> 
-        <TouchableOpacity onPress={() => navigation.navigate('Feed',data )} ><Button style={CardStyle.btn} ><Text style={{letterSpacing: 2,
+        <TouchableOpacity onPress={submit} ><Button style={CardStyle.btn} ><Text style={{letterSpacing: 2,
         color: 'white',
         fontFamily :'poppins-bold', textAlign: 'center' ,
 }}>Connexion</Text></Button></TouchableOpacity> 
